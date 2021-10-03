d99064e3750f9a101de40d1dc451841475de91e0
whitebear <woojs0505@naver.com>
2021-10-03 18:09:28 +0900

    Update: socket.io setting and make room

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,14 +1,15 @@
 const socket = io();
-
 const welcome = document.querySelector("#welcome");
 const form = welcome.querySelector("form");
+
+function backendDone(msg) {
+  console.log("backend  says:", msg);
+}
 
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector("input");
-  socket.emit("enter_room", { payload: input.value }, () => {
-    console.log("server is done!");
-  });
+  socket.emit("enter_room", { payload: input.value }, backendDone);
   input.value = "";
 }
 
--- src/server.js
+++ src/server.js
@@ -18,11 +18,9 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  socket.on("enter_room", (msg, done) => {
-    console.log(msg);
-    setTimeout(() => {
-      done();
-    }, 10000);
+  socket.on("enter_room", (roomName, done) => {
+    console.log(roomName);
+    done("Hello front!!");
   });
 });
 
