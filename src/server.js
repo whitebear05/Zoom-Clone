0deb00a8d7c9bf6de70c6207da678b23e16c4852
whitebear <woojs0505@naver.com>
2021-10-03 17:51:29 +0900

    Update: make room function

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
+const PORT = 3000;
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
@@ -10,10 +11,20 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`✅ Listening on http://localhost:3000`);
+const handleListen = () =>
+  console.log(`✅ Listening on http://localhost:${PORT}`);
 
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 10000);
+  });
+});
 
 // const wss = new WebSocket.Server({ server });
 // const sockets = [];
@@ -37,4 +48,4 @@ const wsServer = SocketIO(httpServer);
 //   });
 // });
 
-httpServer.listen(3000, handleListen);
+httpServer.listen(PORT, handleListen);
